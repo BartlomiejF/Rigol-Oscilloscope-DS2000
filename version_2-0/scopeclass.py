e1343dd58330f7d06205d75366bb4827695528fc
BF <bartlomiej.fliszkiewicz@gmail.com>
2019-08-01 08:48:25 +0200

    bugfixes, improved readibility

--- version_2-0/scopeclass.py
+++ version_2-0/scopeclass.py
@@ -1,15 +1,17 @@
 import os, time
 import numpy as np
 
+
 class usbtmc():
 	"""Simple implementation of a USBTMC device driver"""
  
+
 	def __init__(self, device):
 		self.device = device
 		self.FILE = os.open(device, os.O_RDWR)
  
 	def write(self, command):
-		os.write(self.FILE, command);
+		os.write(self.FILE, command)
  
 	def read(self, length = 950):
 		return os.read(self.FILE, length)
@@ -17,13 +19,15 @@ class usbtmc():
 	def close(self):
 		self.FILE.close()
 
+
 class RigolScope():
 	"""This is Rigol Oscilloscope DS2000 series kind of API
 	It implements function chosen for my project. For more info
 	refer to Rigol DS2000 Programming Guide"""
 	
+
 	def __init__(self,dev):
-		self.scope=usbtmc(dev)
+		self.scope = usbtmc(dev)
 		self.setDefaults()
 		self.readTime()
 		
@@ -35,31 +39,31 @@ class RigolScope():
 	
 	def readTime(self):
 		self.sendCommand(b':WAV:XINC?') #get time increment of horizontal axis
-		xinc=float(self.read())
+		xinc = float(self.read())
 		self.sendCommand(b':TIM:OFFS?') #get the zero position of horizontal axis
-		xoffs=float(self.read())
+		xoffs = float(self.read())
 		self.sendCommand(b':WAV:POIN?') #get number of pointson horizontal axis
-		point_nr=int(self.read())
+		point_nr = int(self.read())
 		self.sendCommand(b':TIM:SCAL?') #get the horizontal scale of small square on the screen (there are 14 of them)
-		xscal=float(self.read())
+		xscal = float(self.read())
 		#below: create numpy array of time points and crop it if there are too much points
-		self.timeAxis=np.arange(-(7-xoffs/xscal)*xscal,14*xscal,xinc)[0:point_nr]
+		self.timeAxis = np.arange( - (7 - xoffs / xscal) * xscal, 14 * xscal, xinc)[0:point_nr]
 		
 	def readDataDefaults(self):
 		"""These are needed to convert waveform data to voltage"""
 		self.sendCommand(b':WAV:YREF?')
-		YRef=float(self.read())
+		YRef = float(self.read())
 		self.sendCommand(b':CHAN1:SCAL?')
-		YScale=float(self.read())
+		YScale = float(self.read())
 		self.sendCommand(b':CHAN1:OFFS?')
-		YOffset=float(self.read())
-		return YRef,YScale,YOffset
+		YOffset = float(self.read())
+		return YRef, YScale, YOffset
 	
 	def sendCommand(self, command):
 		self.scope.write(command=command)
 		
 	def read(self):
-		out=self.scope.read()
+		out = self.scope.read()
 		return out
 	
 	def run(self):
@@ -79,10 +83,10 @@ class RigolScope():
 		self.sendCommand(b':WAV:DATA?')
 		#it seems that regular read(x) function returns finite number of points no matter that the x is very high
 		#so the data must be read in two packages:
-		tempOutput=b''
-		for i in range(2):
-			tempOutput+=self.read()
-		outputData=np.frombuffer(tempOutput[11:],'B') #first 11 bytes are some kind of header or preamble
+		tempOutput = b''
+		for _ in range(2):
+			tempOutput += self.read()
+		outputData = np.frombuffer(tempOutput[11:],'B') #first 11 bytes are some kind of header or preamble
 		return outputData
 		
 	def readAscii(self):
@@ -90,28 +94,28 @@ class RigolScope():
 		self.sendCommand(b':WAV:DATA?')
 		return self.read()
 		
-	def longMeasure(self, lenght, timegap):
-		output=np.ndarray(self.timeAxis.shape)
-		for i in np.arange(0, length*60, timegap):
-			start=time.time()
-			output=np.vstack((output,readData())) #now every row will be subsequent read data
-			to_wait=timegap-(time.time()-start)
+	def longMeasure(self, length, timegap):
+		output = np.ndarray(self.timeAxis.shape)
+		for _ in np.arange(0, length*60, timegap):
+			start = time.time()
+			output = np.vstack((output, self.readBinData())) #now every row will be subsequent read data
+			to_wait = timegap-(time.time() - start)
 			time.sleep(to_wait)
-		result=self.cvtData2Voltage(output[:,1])
-		columns=np.arange(0,length*60,timegap).astype('str')
-		columns=list(np.core.defchararray.add(columns, np.array('s')))
+		result = self.cvtData2Voltage(output[:,1])
+		columns = np.arange(0,length*60,timegap).astype('str')
+		columns = list(np.core.defchararray.add(columns, np.array('s')))
 		return result, columns
 		
 	def cvtData2Voltage(self, data):
 		"""data must be numpy array or pandas dataframe
 		the conversion formula was taken from scope programming guide, page 572
-    http://int.rigol.com/Support/Manual/5 """
-		YRef,YScale,YOffset=self.readDataDefaults()
-		result=(data-YRef)*YScale-YOffset
+        http://int.rigol.com/Support/Manual/5 """
+		YRef,YScale,YOffset = self.readDataDefaults()
+		result = (data-YRef) * YScale - YOffset
 		return result
 		
 	def setAvgs(self, num):
-		self.sendCommand(bytearray(':ACQ:AVER '+num, encoding='utf-8'))
+		self.sendCommand(bytearray(':ACQ:AVER ' + num, encoding='utf-8'))
 		
 	def finish(self):
 		self.scope.close()
